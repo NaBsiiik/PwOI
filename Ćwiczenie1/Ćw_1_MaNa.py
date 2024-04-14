a52697fb9b84b46cabbbf83d25678deea0e1e4a1
NaBsiiik <matnab@wp.pl>
2024-04-14 16:53:00 +0200

    Git

--- Ćwiczenie1/Ćw_1_MaNa.py
+++ Ćwiczenie1/Ćw_1_MaNa.py
@@ -4,7 +4,7 @@ from scipy.stats import norm
 from csv import writer
 import numpy as np
 
-def generate_points_flat_horizontal(num_points: int = 2000, width: float = 100, length: float = 100, height: float = 10):
+def generate_points_flat_horizontal(num_points: int = 2000, width: float = 100, length: float = 100, height: float = 1):
     distribution_x = norm(loc=0, scale=width)
     distribution_y = norm(loc=0, scale=length)
 
@@ -15,7 +15,7 @@ def generate_points_flat_horizontal(num_points: int = 2000, width: float = 100, 
     points = zip(x, y, z)
     return points
 
-def generate_points_flat_vertical(num_points: int = 2000, width: float = 100, height: float = 100, length: float = 10):
+def generate_points_flat_vertical(num_points: int = 2000, width: float = 100, height: float = 100, length: float = 1):
     distribution_x = norm(loc=0, scale=width)
     distribution_z = norm(loc=0, scale=height)
 
@@ -26,7 +26,7 @@ def generate_points_flat_vertical(num_points: int = 2000, width: float = 100, he
     points = zip(x, y, z)
     return points
 
-def generate_points_cylindrical(num_points: int = 2000, radius: float = 50, height: float = 100):
+def generate_points_cylindrical_vertical(num_points: int = 2000, radius: float = 40, height: float = 200):
     angle = np.random.uniform(0, 2 * np.pi, num_points)
     radius_sample = np.random.uniform(0, radius, num_points)
     height_sample = np.random.uniform(0, height, num_points)
@@ -34,6 +34,18 @@ def generate_points_cylindrical(num_points: int = 2000, radius: float = 50, heig
     x = radius_sample * np.cos(angle)
     y = radius_sample * np.sin(angle)
     z = height_sample
+
+    points = zip(x, y, z)
+    return points
+
+def generate_points_cylindrical_horizontal(num_points: int = 2000, radius: float = 40, length: float = 200):
+    angle = np.random.uniform(0, 2 * np.pi, num_points)
+    radius_sample = np.random.uniform(0, radius, num_points)
+    length_sample = np.random.uniform(0, length, num_points)
+
+    x = radius_sample * np.cos(angle)
+    z = radius_sample * np.sin(angle)
+    y = length_sample
 
     points = zip(x, y, z)
     return points
@@ -46,13 +58,19 @@ def save_points_to_csv(points, filename):
 
 if __name__ == '__main__':
     # a. płaska pozioma powierzchnia o ograniczonej szerokości i długości
-    cloud_points = generate_points_flat_horizontal(2000, width=100, length=100, height=10)
+    cloud_points = generate_points_flat_horizontal(2000, width=100, length=100, height=1)
     save_points_to_csv(cloud_points, 'CsvData_flat_horizontal.xyz')
 
     # b. płaska pionowa powierzchnia o ograniczonej szerokości i wysokości
-    cloud_points = generate_points_flat_vertical(2000, width=100, height=100, length=10)
+    cloud_points = generate_points_flat_vertical(2000, width=100, height=100, length=1)
     save_points_to_csv(cloud_points, 'CsvData_flat_vertical.xyz')
 
-    # c. powierzchnia cylindryczna o zadanym promieniu i ograniczonej wysokości
-    cloud_points = generate_points_cylindrical(2000, radius=40, height=200)
-    save_points_to_csv(cloud_points, 'CsvData_cylindrical.xyz')
+    # c. powierzchnia cylindryczna - pozioma o zadanym promieniu i ograniczonej wysokości
+    cloud_points = generate_points_cylindrical_horizontal(2000, radius=50, length=150)
+    save_points_to_csv(cloud_points, 'CylindricalData_horizontal.xyz')
+
+    # d. powierzchnia cylindryczna - pionowa o zadanym promieniu i ograniczonej wysokości
+    cloud_points = generate_points_cylindrical_vertical(2000, radius=50, height=150)
+    save_points_to_csv(cloud_points, 'CylindricalData_vertical.xyz')
+
+
